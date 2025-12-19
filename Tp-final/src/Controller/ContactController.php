9425e3031cc5f04bc393fe1e1c9d8858092457d6
k-emmanuel <emmanuelmakosso00@gmail.com>
2025-12-19 09:08:20 +0100

    page contact

---
+++ Tp-final/src/Controller/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request): Response
+    {
+        if ($request->isMethod('POST')) {
+            $name = $request->request->get('name');
+            $email = $request->request->get('email');
+            $message = $request->request->get('message');
+
+            // Simuler l'envoi du message
+            $this->addFlash('success', 'Votre message a été envoyé avec succès !');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/index.html.twig');
+    }
+}
