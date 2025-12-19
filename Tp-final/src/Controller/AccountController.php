bdf9de8048698bfe917b91b563ab96039d2eecfc
k-emmanuel <emmanuelmakosso00@gmail.com>
2025-12-19 10:00:53 +0100

    ajout des boutons modifier et suprimer

--- Tp-final/src/Controller/AccountController.php
+++ Tp-final/src/Controller/AccountController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -18,5 +21,38 @@ class AccountController extends AbstractController
         return $this->render('account/index.html.twig', [
             'user' => $user,
         ]);
+    }
+
+    #[Route('/compte/modifier', name: 'app_account_edit', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $username = $request->request->get('username');
+
+        if ($username) {
+            $user->setUsername($username);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre nom a été modifié avec succès !');
+        }
+
+        return $this->redirectToRoute('app_account');
+    }
+
+    #[Route('/compte/supprimer', name: 'app_account_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function delete(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $request->getSession()->invalidate();
+        $this->container->get('security.token_storage')->setToken(null);
+
+        $this->addFlash('success', 'Votre compte a été supprimé avec succès.');
+
+        return $this->redirectToRoute('app_home');
     }
 }
